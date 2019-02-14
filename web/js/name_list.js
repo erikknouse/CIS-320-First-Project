c9a26f7ade5547ce88e0f4d984471891daf30cd0
Erik Knouse <erik.knouse@my.simpson.edu>
2019-02-14 09:20:00 -0600

    Lab5

--- web/js/name_list.js
+++ web/js/name_list.js
@@ -1,5 +1,45 @@
 // Main Javascript File
 
+function validateFirstName(){
+    var regex = /^[A-Za-z]+(((\'|\-|\.)?([A-Za-z])+))?$/
+    if(regex.test($('#firstName').val())){
+        $('#firstName').removeClass("is-invalid");
+        $('#firstName').addClass("is-valid");
+    }
+}
+
+function validateLastName(){
+    var regex = /^[A-Za-z]+(((\'|\-|\.)?([A-Za-z])+))?$/
+    if(regex.test($('#lastName').val())){
+        $('#lastName').removeClass("is-invalid");
+        $('#lastName').addClass("is-valid");
+    }
+}
+
+function validateEmail(){
+    var regex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+    if(regex.test($('#email').val())){
+        $('#email').removeClass("is-invalid");
+        $('#email').addClass("is-valid");
+    }
+}
+function validatePhone(){
+    var regex = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/
+    if(regex.test($('#phone').val())){
+        $('#phone').removeClass("is-invalid");
+        $('#phone').addClass("is-valid");
+    }
+}
+
+function validateBirthday(){
+    var regex = /^\d{2}\/\d{2}\/\d{4}$/
+    if(regex.test($('#birthday').val())){
+        $('#birthday').removeClass("is-invalid");
+        $('#birthday').addClass("is-valid");
+    }
+}
+
+
 function updateTable() {
     var url = "api/name_list_get";
 
@@ -22,3 +62,39 @@ function updateTable() {
 
 // Call your code.
 updateTable();
+
+var addItemButton = $('#addItem');
+addItemButton.on("click", showDialogAdd);
+
+function showDialogAdd() {
+
+    // Print that we got here
+    console.log("Opening add item dialog");
+
+    // Clear out the values in the form.
+    // Otherwise we'll keep values from when we last
+    // opened or hit edit.
+    // I'm getting it started, you can finish.
+    $('#id').val("");
+    $('#firstName').val("");
+    $('#lastName').val("");
+    $('#email').val("");
+    $('#phone').val("");
+    $('#birthday').val("");
+
+    // Show the hidden dialog
+    $('#myModal').modal('show');
+}
+
+var saveChangesButton = $('#saveChanges');
+saveChangesButton.on("click", showSaveChanges);
+
+function showSaveChanges(){
+    validateFirstName();
+    validateLastName();
+    validateEmail();
+    validatePhone();
+    validateBirthday();
+    console.log("Saving Changes")
+
+}
